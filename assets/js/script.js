dd51ad02428e9e548f7931545d0bb0840aa6f820
Sheryl L <s_lawrence0@yahoo.ca>
2024-01-22 16:26:55 -0500

    js refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-//   console.log("Does this work?");
+function generatePassword(length) {
+//    console.log("Does this work?");
 
-  var passwordLength = 10;
-  var possibleCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  var password = "";
+   var passwordLength = 8;
+   var possibleCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+   var password = "";
+
+   for (var i = 0; i <= password.length; i++) { 
+    // console.log("I am going to zoo to see " + zooAnimals[i] + ".");
+    var index = Math.floor(Math.random() * password.length);
+    }
+}
+
+
+// We use a for-loop to execute code more than once
+for (var i = 0; i < 5; i++) {
+    // This is the block of code that will run each time
+    console.log("This is the current value of i: " + i + ".");
+}
+
+// For-loops are often used to iterate over arrays
+var zooAnimals = ["Bears", "Giraffes", "Penguins", "Meerkats"];
+
+//To determine how many times the loop should execute, we use the array's length
+for (var i = 0; i < zooAnimals.length; i++) { 
+    console.log("I am going to zoo to see " + zooAnimals[i] + ".");
+}
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCase = ["a", "b", "c", "d", "e"];
 
 //  Password Criteria
 //  Length (min. 8 - max. 128)
@@ -14,7 +37,7 @@ function generatePassword() {
 //  Uppercase (ABCDEFGHIJKLMNOPQRSTUVWXYZ) = Include or not 
 //  Numbers (0123456789) = Include or not
 
-// var passwordLength = 8 <= 128;
+// var passwordLength = ;
 // var Lowercase = abcdefghijklmnopqrstuvwxyz
 // var Uppercase = ABCDEFGHIJKLMNOPQRSTUVWXYZ
 // var Numbers = 0123456789
@@ -30,7 +53,7 @@ function generatePassword() {
 
 //   if(passwordLength <= 7){
     // return ("Password length must be at least 8 characters");
-  }
+
 
 //   if(passwordLength >= 129){
     // return ("Password length must be no more than 128 characters");
@@ -50,19 +73,15 @@ function generatePassword() {
     // console.log('They are not equal');
 //   }
 
-for (var i = 0; i <= password.length; i++) { 
-     // console.log("I am going to zoo to see " + zooAnimals[i] + ".");
- }
-
-var index = Math.floor(Math.random() * password.length);
-
+ 
+// window.alert("The computer chose " + computerChoice);
 //  window.alert("The computer chose " + computerChoice);
 //   return ;
 //  }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword("");
+  var password = generatePassword(length);
     var passwordText = document.querySelector("#password");
 
 
