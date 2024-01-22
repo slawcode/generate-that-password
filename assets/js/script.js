c48b8abdfab4037fdd28c362186fee0f504c313e
Sheryl L <s_lawrence0@yahoo.ca>
2024-01-22 17:31:34 -0500

    js refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,38 +4,44 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(length) {
 //    console.log("Does this work?");
 
-   var passwordLength = 8;
-   var possibleCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
    var password = "";
+   var passwordLength = 8 <= 128;
+   var possibleCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-   for (var i = 0; i <= password.length; i++) { 
-    // console.log("I am going to zoo to see " + zooAnimals[i] + ".");
+   for (var i = 8; i <= 128; i++) { 
     var index = Math.floor(Math.random() * password.length);
     }
 }
 
-
-// We use a for-loop to execute code more than once
-for (var i = 0; i < 5; i++) {
-    // This is the block of code that will run each time
-    console.log("This is the current value of i: " + i + ".");
-}
-
-// For-loops are often used to iterate over arrays
-var zooAnimals = ["Bears", "Giraffes", "Penguins", "Meerkats"];
-
-//To determine how many times the loop should execute, we use the array's length
-for (var i = 0; i < zooAnimals.length; i++) { 
-    console.log("I am going to zoo to see " + zooAnimals[i] + ".");
-}
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerCase = ["a", "b", "c", "d", "e"];
-
-//  Password Criteria
+// Password Criteria
 //  Length (min. 8 - max. 128)
 //  Lowercase (abcdefghijklmnopqrstuvwxyz) = Include or not
 //  Uppercase (ABCDEFGHIJKLMNOPQRSTUVWXYZ) = Include or not 
 //  Numbers (0123456789) = Include or not
+
+
+// We use a for-loop to execute code more than once
+
+
+// For-loops are often used to iterate over arrays
+// var zooAnimals = ["Bears", "Giraffes", "Penguins", "Meerkats"];
+
+//To determine how many times the loop should execute, we use the array's length
+// for (var i = 0; i < zooAnimals.length; i++) { 
+    // console.log("I am going to zoo to see " + zooAnimals[i] + ".");
+// }
+// var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var lowerCase = ["a", "b", "c", "d", "e"];
+
+// for (var i = 0; i < 10; i++) {
+    // This is the block of code that will run each time
+    // console.log("This is the current value of i: " + i + ".");
+// }
+// (var i = 0; i < 10; i++) {
+    // This is the block of code that will run each time
+    // console.log("This is the current value of i: " + i + ".");
+// }
+
 
 // var passwordLength = ;
 // var Lowercase = abcdefghijklmnopqrstuvwxyz
@@ -54,14 +60,8 @@ var lowerCase = ["a", "b", "c", "d", "e"];
 //   if(passwordLength <= 7){
     // return ("Password length must be at least 8 characters");
 
-
 //   if(passwordLength >= 129){
     // return ("Password length must be no more than 128 characters");
-//   }
-
-// Evaluates to false so nothing is logged
-// if (hungerLevel < 50) {
-    // console.log("I'm full!");
 //   }
 
 // var isEqualTakeTwo = function (x, y) 
@@ -73,15 +73,9 @@ var lowerCase = ["a", "b", "c", "d", "e"];
     // console.log('They are not equal');
 //   }
 
- 
-// window.alert("The computer chose " + computerChoice);
-//  window.alert("The computer chose " + computerChoice);
-//   return ;
-//  }
-
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(length);
+  var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
 
