476848ce1b9d1966ed0d1295030f96e2011ae2ec
Sheryl L <s_lawrence0@yahoo.ca>
2024-01-22 23:20:20 -0500

    final edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,20 @@
 
 var generateBtn = document.querySelector("#generate");
 
-//  generatePassword function created 
+//  generatePassword function created to generate a password
 function generatePassword(length) {
 
-   var password = "";
-   var passwordLength = 8 <= 128;
-   var possibleCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-// A for-loop to execute code more than once
-for (var i = 8; i <= 128; i++) { 
-    var randonIndex = Math.floor(Math.random() * length.characters);
-    }
-}
+//    var password = "";
+//    var passwordLength = 8 <= 128;
+//    var possibleCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 // var passwordLength = ;
 // var Lowercase = abcdefghijklmnopqrstuvwxyz
 // var Uppercase = ABCDEFGHIJKLMNOPQRSTUVWXYZ
 // var Numbers = 0123456789
 
-  var password = 
+// Password variable criteria
+var password = 
     length: 8 <= 128,
     includesLoweCase: true or false,
     lowerCase: "abcdefghijklmnopqrstuvwxyz",
@@ -44,48 +39,19 @@ for (var i = 8; i <= 128; i++) {
     return ("Password length must be no more than 128 characters"); 
    }
 
-
-
-// For-loops are often used to iterate over arrays
-// var zooAnimals = ["Bears", "Giraffes", "Penguins", "Meerkats"];
-
-//To determine how many times the loop should execute, we use the array's length
-// for (var i = 0; i < zooAnimals.length; i++) { 
-    // console.log("I am going to zoo to see " + zooAnimals[i] + ".");
-// }
-// var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// var lowerCase = ["a", "b", "c", "d", "e"];
-
-// for (var i = 0; i < 10; i++) {
-    // This is the block of code that will run each time
-    // console.log("This is the current value of i: " + i + ".");
-// }
-// (var i = 0; i < 10; i++) {
-    // This is the block of code that will run each time
-    // console.log("This is the current value of i: " + i + ".");
-// }
-
-
-
-// var isEqualTakeTwo = function (x, y) 
-//   if (x === y) {
-    // console.log('They are equal in type and value');
-//   } else if (x == y) {
-    // console.log('They are equal in value');
-//   } else {
-    // console.log('They are not equal');
-//   }
-
-
+   // A for-loop to execute code more than once
+for (var i = 8; i <= 128; i++) { 
+    var randonIndex = Math.floor(Math.random() * length.characters);
+    }
 
 // Write password to the #password input
 
-// Calling the function generatePassword and storing value for that function into the password variable 
+// Calling the function generatePassword and storing value for that function into the password variable for the password display
 function writePassword() {
   var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-   passwordText.value = password;
+  passwordText.value = password;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
